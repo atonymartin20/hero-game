18ef4a53e305fa33e575a97af587237973afb1cd
Alex Martin <atonymartin20@yahoo.com>
2020-06-05 18:00:20 -0500

    Added heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
