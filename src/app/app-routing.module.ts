b6826e46b682b16976236ddf1bfbc07eae3a1ae2
Alex Martin <atonymartin20@yahoo.com>
2020-06-04 22:54:54 -0500

    Added dashboard Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent }
   // { path: '**', component: MAKE404COMPONENT }, //Wildcard Route for a 404 page
 ]
 
