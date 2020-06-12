0cadc4022dce4531efa02950a4deaf4c2ad68798
Alex Martin <atonymartin20@yahoo.com>
2020-06-11 23:17:01 -0500

    Adding hero now closes add hero component after running

--- src/app/add-hero/add-hero.component.ts
+++ src/app/add-hero/add-hero.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroesComponent } from '../heroes/heroes.component';
 
 @Component({
   selector: 'app-add-hero',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-hero.component.css']
 })
 export class AddHeroComponent implements OnInit {
+  hero: Hero = {
+    id: HeroesComponent.length,
+    name: '',
+    atk: 3,
+    def: 3,
+    hp: 30,
+    lvl: 1
+  };
+  error: boolean
 
   constructor() { }
-
+  
   ngOnInit(): void {
+  }
+
+  @Output() heroSubmit: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  addHero(): void {
+    if(this.hero.name !== '') {
+      console.log(this.hero.id, this.hero.name, this.hero.atk, this.hero.def, this.hero.hp, this.hero.lvl);
+      this.heroSubmit.emit(false);
+    }
+    else {
+      this.error = true;
+      console.log('name is empty')
+    }
   }
 
 }
