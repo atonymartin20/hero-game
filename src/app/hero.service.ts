c283da36b845d29b22500a55d0b3040e8157f71f
Alex Martin <atonymartin20@yahoo.com>
2020-06-18 22:47:14 -0500

    Update hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Hero } from './hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl= 'api/heroes';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Context-Type': 'application/json' })
+  }
+
+  constructor( private http: HttpClient, private messageService: MessageService) { }
+
+
 }
