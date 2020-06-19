8b7b4356000a50a9cd7845ba211cfd7d8b7fbaaa
Alex Martin <atonymartin20@yahoo.com>
2020-06-18 22:49:41 -0500

    Update hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,5 +18,10 @@ export class HeroService {
 
   constructor( private http: HttpClient, private messageService: MessageService) { }
 
-
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    )
+  }
 }
