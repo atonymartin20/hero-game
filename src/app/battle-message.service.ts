2ec4fb268d46b6f291e262ff73ce3966449aa6c5
Alex Martin <atonymartin20@yahoo.com>
2020-06-20 22:42:33 -0500

    Update battle-message.service.ts

--- src/app/battle-message.service.ts
+++ src/app/battle-message.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BattleMessageService {
-
+  messages: string[] = [];
   constructor() { }
+
+  add (message: string) {
+    this.messages.push(message)
+  }
+
+  clear() {
+    this.messages = [];
+  }
+  
 }
