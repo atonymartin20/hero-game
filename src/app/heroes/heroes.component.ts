55ab875d7e2a6c75024b1399c5a668383784b573
Alex Martin <atonymartin20@yahoo.com>
2020-06-21 22:41:18 -0500

    Delete hero works

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -49,6 +49,8 @@ export class HeroesComponent implements OnInit {
     // this.heroService.addHero({ name: name, atk: attack, def: defense, hp: health, lvl: level } as Hero).subscribe(hero => { this.heroes.push(hero)});
   }
 
-  delete(hero): void {
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
