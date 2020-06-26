e05a508e1545e39f07470bcc2dac120cb6c058c3
Alex Martin <atonymartin20@yahoo.com>
2020-06-26 12:44:00 -0500

    Battle Component Functionality added

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-battle',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./battle.component.css']
 })
 export class BattleComponent implements OnInit {
+  heroes: Hero[];
+  attackingHero: Hero[];
+  defendingHero: Hero[];
+  attackingHeroSelected: boolean = false;
+  defendingHeroSelected: boolean = false;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    console.log('Getting Battle Heroes')
+  }
+
+  battle(): void {
+    console.log(this.attackingHero, this.defendingHero);
+    console.log('Hero A vs Hero B')
+  }
+
+  selectAttackingHero(hero): void {
+    this.attackingHero = hero
+    console.log(this.attackingHero)
+    this.attackingHeroSelected = true;
+  }
+
+  resetAttackingHero(): void {
+    this.attackingHero = null;
+    this.attackingHeroSelected = false;
+  }
+
+  selectDefendingHero(hero): void {
+    this.defendingHero = hero
+    console.log(this.defendingHero)
+    this.defendingHeroSelected = true;
+  }
+
+  resetDefendingHero(): void {
+    this.defendingHero = null;
+    this.defendingHeroSelected = false;
+  }
 }
