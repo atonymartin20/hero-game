7944fcf821d8d51fbd7eaf180e20e190a0dea4cf
Alex Martin <atonymartin20@yahoo.com>
2020-06-27 08:50:56 -0500

    Finished battle function

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -3,52 +3,131 @@ import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-battle',
-  templateUrl: './battle.component.html',
-  styleUrls: ['./battle.component.css']
+    selector: 'app-battle',
+    templateUrl: './battle.component.html',
+    styleUrls: ['./battle.component.css'],
 })
 export class BattleComponent implements OnInit {
-  heroes: Hero[];
-  attackingHero: Hero[];
-  defendingHero: Hero[];
-  attackingHeroSelected: boolean = false;
-  defendingHeroSelected: boolean = false;
+    heroes: Hero[];
+    attackingHero: Hero[];
+    defendingHero: Hero[];
+    attackingHeroSelected: boolean = false;
+    defendingHeroSelected: boolean = false;
 
-  constructor(private heroService: HeroService) { }
+    constructor(private heroService: HeroService) {}
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
 
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-    console.log('Getting Battle Heroes')
-  }
+    getHeroes(): void {
+        this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+        console.log('Getting Battle Heroes');
+    }
 
-  battle(): void {
-    console.log(this.attackingHero, this.defendingHero);
-    console.log('Hero A vs Hero B')
-  }
+    battle(): void {
+        this.heroService.clearMessages();
+       
+        if (this.attackingHero !== null && this.defendingHero !== null) {
+            let attackersHP = this.attackingHero['hp'];
+            let defendersHP = this.defendingHero['hp'];
+            let criticalHitPercentage = 0.1;
 
-  selectAttackingHero(hero): void {
-    this.attackingHero = hero
-    console.log(this.attackingHero)
-    this.attackingHeroSelected = true;
-  }
+            this.heroService.log(`${this.attackingHero['name']} vs ${this.defendingHero['name']}`);
+            while (attackersHP > 0 && defendersHP > 0) {
+                if (attackersHP > 0) {
+                    if (Math.random() < criticalHitPercentage) {
+                        let damage = Math.floor(this.attackingHero['atk'] * 1.5 - this.defendingHero['def']);
+                        if (defendersHP <= damage) {
+                            this.heroService.log(`${this.attackingHero['name']} lands a critical hit finishing off ${this.defendingHero['name']}.`);
+                            this.heroService.log(`${this.attackingHero['name']} wins.`)
+                            defendersHP -= damage;
+                            return null;
+                        } else {
+                            defendersHP -= damage;
+                            this.heroService.log(`${this.attackingHero['name']} lands a critical hit dealing ${damage} damage.`);
+                            this.heroService.log(`${this.defendingHero['name']} has ${defendersHP} HP remaining.`);
+                        }
+                    } else {
+                        let damage = this.attackingHero['atk'] - this.defendingHero['def'];
+                        if (defendersHP <= damage) {
+                            this.heroService.log(`${this.attackingHero['name']} lands the final blow on ${this.defendingHero['name']}.`);
+                            this.heroService.log(`${this.attackingHero['name']} wins.`)
+                            defendersHP -= damage;
+                            return null;
+                        } else {
+                            defendersHP -= damage;
+                            if (Math.random() < .33) {
+                                this.heroService.log(`${this.attackingHero['name']} attacks dealing ${damage} damage.`);
+                            }
+                            else if (Math.random() < .67) {
+                                this.heroService.log(`${this.attackingHero['name']} strikes dealing ${damage} damage.`);
+                            }
+                            else {
+                                this.heroService.log(`${this.attackingHero['name']} charges dealing ${damage} damage.`);
+                            }
+                            this.heroService.log(`${this.defendingHero['name']} has ${defendersHP} HP remaining.`);
+                        }
+                    }
+                }
+                if (defendersHP > 0) {
+                    if (Math.random() < criticalHitPercentage) {
+                        let damage = Math.floor(this.defendingHero['atk'] * 1.5 - this.attackingHero['def']);
+                        if (attackersHP <= damage) {
+                            this.heroService.log(`${this.defendingHero['name']} lands a critical hit finishing off ${this.attackingHero['name']}.`);
+                            this.heroService.log(`${this.defendingHero['name']} wins.`)
+                            attackersHP -= damage;
+                            return null;
+                        } else {
+                            attackersHP -= damage;
+                            this.heroService.log(`${this.defendingHero['name']} lands a critical hit dealing ${damage} damage.`);
+                            this.heroService.log(`${this.attackingHero['name']} has ${attackersHP} HP remaining.`);
+                        }
+                    } else {
+                        let damage = this.defendingHero['atk'] - this.attackingHero['def'];
+                        if (attackersHP <= damage) {
+                            this.heroService.log(`${this.defendingHero['name']} lands the final blow on ${this.attackingHero['name']}.`);
+                            this.heroService.log(`${this.defendingHero['name']} wins.`)
+                            attackersHP -= damage;
+                            return null;
+                        } else {
+                            attackersHP -= damage;
+                            if (Math.random() < .33) {
+                                this.heroService.log(`${this.defendingHero['name']} attacks dealing ${damage} damage.`);
+                            }
+                            else if (Math.random() < .67) {
+                                this.heroService.log(`${this.defendingHero['name']} strikes dealing ${damage} damage.`);
+                            }
+                            else {
+                                this.heroService.log(`${this.defendingHero['name']} charges dealing ${damage} damage.`);
+                            }
+                            this.heroService.log(`${this.attackingHero['name']} has ${attackersHP} HP remaining.`);
+                        }
+                    }
+                }
+            }
+        } else {
+            return null;
+        }
+    }
 
-  resetAttackingHero(): void {
-    this.attackingHero = null;
-    this.attackingHeroSelected = false;
-  }
+    selectAttackingHero(hero): void {
+        this.attackingHero = hero;
+        this.attackingHeroSelected = true;
+    }
 
-  selectDefendingHero(hero): void {
-    this.defendingHero = hero
-    console.log(this.defendingHero)
-    this.defendingHeroSelected = true;
-  }
+    resetAttackingHero(): void {
+        this.attackingHero = null;
+        this.attackingHeroSelected = false;
+    }
 
-  resetDefendingHero(): void {
-    this.defendingHero = null;
-    this.defendingHeroSelected = false;
-  }
+    selectDefendingHero(hero): void {
+        this.defendingHero = hero;
+        this.defendingHeroSelected = true;
+    }
+
+    resetDefendingHero(): void {
+        this.defendingHero = null;
+        this.defendingHeroSelected = false;
+    }
 }
